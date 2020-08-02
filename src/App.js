6e5f9f47b2bed9b6a6618bcab93b41564a8c7759
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-08-02 16:19:47 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import CatComponent	from './CatComponent.js';
+//why does one component have to be in brackets and the other does not?
+import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
+// import React, { CatComponent } from 'react';
+// import React, { GraceHopperQuoteComponent } from 'react';
 
 class App extends Component {
 	render() {
@@ -7,6 +13,7 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
+				<MouseComponent />
 				{/* one more component missing */}
 			</div>
 		);
